ec6c2c5ec1d721b1d1c543f5da32ed4d5e1200e1
Erik Mendoza <efernandomendoza@gmail.com>
2023-03-01 13:48:47 -0600

    Fix class-validator version issue

--- src/matches/dtos/addMatchResult.dto.ts
+++ src/matches/dtos/addMatchResult.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsString } from 'class-validator';
 
 export class AddMatchResult {
   @ApiProperty()
   @IsString()
+  @Type(() => String)
   contenderId: string;
 }
